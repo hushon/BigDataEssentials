e4c462119709a594d918ce7ad646de4bee229dc1
hushon <hyounguk.shon@gmail.com>
2019-10-28 22:28:10 +0900

    updated

--- 4_kMeansClustering/kmeans.py
+++ 4_kMeansClustering/kmeans.py
@@ -105,17 +105,17 @@ class Cluster:
 
     @property
     def diameter(self):
+        result = 0.0
         if len(self) > 1:
-            result = max([distance(x, y) for x, y in itertools.combinations(self.points, 2)])
-        else:
-            result = 0.0
+            # result = max([distance(x, y) for x, y in itertools.combinations(self.points, 2)])
+            for x, y in itertools.combinations(self.points, 2):
+                result = max(result, distance(x, y))
         return result
 
     @property
     def variance(self):
         N = len(self)
         return (self._SUMSQ/N) - (self._SUM/N)**2
-    
 
 def find_centroid(x, y):
     '''
@@ -148,8 +148,8 @@ def main():
     clusters = [Cluster() for _ in range(k)]
 
     # add each centroid to each cluster
+    assert len(centroids) == len(clusters)
     for i, centroid in enumerate(centroids):
-        clusters[i].add_point(centroid)
         clusters[i].centroid = centroid
 
     ''' k-means algorithm '''
@@ -159,37 +159,44 @@ def main():
     #     Add p to the cluster of that centroid;
     # END;
 
-    clusters = sc.parallelize(points, nExecuter)
-    clusters = clusters.flatMap(lambda p: [(tuple(p), (i, distance(p, centroids[i]))) for i in range(len(centroids))])
-    clusters = clusters.reduceByKey(lambda x, y: x if x[1] < y[1] else y)
+    points_RDD = sc.parallelize(points, nExecuter)
+    points_RDD = points_RDD.flatMap(lambda p: [(tuple(p), (i, distance(p, centroids[i]))) for i in range(len(centroids))])
+    points_RDD = points_RDD.reduceByKey(lambda x, y: x if x[1] < y[1] else y)
 
-    clusters = clusters.map(lambda (p, (i, d)): (i, p))
-    clusters = clusters.groupByKey()
+    points_RDD = points_RDD.map(lambda (p, (i, d)): (i, p))
+    points_RDD = points_RDD.groupByKey()
 
-    # calculate diameter of each cluster by spark
-    for cluster in clusters.collect():
-        i, points = cluster
-        print len(points)
-        if len(points) > 1:
-            points_RDD = sc.parallelize([(p1, p2) for p1, p2 in itertools.combinations(points, 2)], nExecuter)
-            points_RDD = points_RDD.map(lambda (p1, p2): distance(p1, p2))
-            points_RDD = points_RDD.reduce(lambda x, y: max(x, y))
-            print points_RDD.collect()
-        else:
-            diameter = 0
+    # add points to corresponding clusters
+    for i, points in points_RDD.collect():
+        for point in points:
+            clusters[i].add_point(point)
 
-    # for p, (c, _) in points.collect():
-    #     for cluster in clusters:
-    #         if np.allclose(cluster.centroid, c):
-    #             cluster.add_point(p)
-
-
-    # print [len(c) for c in clusters]
+    # points_RDD = points_RDD.flatMap(lambda (i, p_list): [(i, (p1, p2)) for p1, p2 in itertools.combinations(p_list, 2)])
+    # points_RDD = points_RDD.map(lambda (i, (p1, p2)): (i, distance(p1, p2)))
+    # points_RDD = points_RDD.reduceByKey(lambda x, y: max(x, y))
+    # diameter = points_RDD.collect()
+    # print 'done'
+    # # calculate diameter of each cluster by spark
+    # for i, points in points_RDD.collect():
+    #     print len(points)
+    #     if len(points) > 1:
+    #         points_RDD = sc.parallelize([(p1, p2) for p1, p2 in itertools.combinations(points, 2)], nExecuter)
+    #         points_RDD = points_RDD.map(lambda (p1, p2): distance(p1, p2))
+    #         points_RDD = points_RDD.reduce(lambda x, y: max(x, y))
+    #         diameter = points_RDD.collect()
+    #     else:
+    #         diameter = 0
+    #     print diameter
 
     ''' print average diameter of clusters '''
-    # this seems too expensive
-    # print np.average([c.diameter for c in clusters])
-
+    print 'k-value: {}'.format(k)
+    d_accumulator = 0.0
+    for i, c in enumerate(clusters):
+        d = c.diameter
+        d_accumulator += d
+        print 'Cluster #{}\t points {} \t diameter {}'.format(i, len(c), c.diameter)
+    print 'Average diameter: {}'.format(d_accumulator/k)
+    
 if __name__ == '__main__':
 
     ''' sanity check '''
--- 6_RecommendationSystems/collaborativefiltering.py
+++ 6_RecommendationSystems/collaborativefiltering.py
@@ -11,7 +11,6 @@ Example text file: http://www.di.kaist.ac.kr/~swhang/ee412/ratings.txt
 
 import sys
 import os
-from pyspark import SparkConf, SparkContext
 import numpy as np
 from numpy.linalg import norm
 import time
@@ -39,11 +38,12 @@ def cosine(x, y):
     Return:
         Cosine distance between x, y
     '''
-    x = np.nan_to_num(x).flatten()
-    y = np.nan_to_num(y).flatten()
-    assert len(x) == len(y)
-    result = np.dot(x, y) / (norm(x) * norm(y))
-    assert not np.isnan(result)
+    if np.all(np.isnan(x)) or np.all(np.isnan(y)):
+        return np.nan
+    else:
+        x = np.nan_to_num(x).flatten()
+        y = np.nan_to_num(y).flatten()
+        return np.dot(x, y) / (norm(x) * norm(y))
     return result
 
 def mean(x):
@@ -51,7 +51,8 @@ def mean(x):
     Take mean of vector, but ignore np.nan values.
     '''
     x = np.array(x).flatten()
-    return np.mean([v for v in x if not np.isnan(v)])
+    result = np.nanmean(x)
+    return result
 
 def normalize_utility_matrix(M):
     '''
@@ -61,12 +62,15 @@ def normalize_utility_matrix(M):
     Return:
         Normalized M
     '''
-    means = []
-    for i in range(M.shape[0]):
-        means.append(mean(M[i, :]))
-        M[i, :] = M[i, :] - means[i]
-    assert len(means) == M.shape[0]
-    return M, np.array(means)
+    # M = M.copy()
+    # means = []
+    # for i in range(M.shape[0]):
+    #     means.append(mean(M[i, :]))
+    #     M[i, :] = M[i, :] - means[i]
+    # assert len(means) == M.shape[0]
+    # return M, np.array(means)
+
+    return M - np.nanmean(M, axis=1)[..., np.newaxis]
 
 def denormalize_utility_matrix(M, means):
     '''
@@ -83,61 +87,66 @@ def denormalize_utility_matrix(M, means):
 def main():
     ''' parameters '''
     filepath = sys.argv[1]
-    nExecuter = 8
-    conf = SparkConf()
-    sc = SparkContext(conf=conf)
 
     ''' read and parse dataset '''
     with open(filepath, 'r') as file:
         lines = file.readlines()
 
     ratings = map(parse, lines)
-    
-    # extract distinct userIDs and movieIDs
-    ratings_rdd = sc.parallelize(ratings, nExecuter)
-    user_list = ratings_rdd.map(lambda (u,m,r,t): u).distinct().collect()
-    movie_list = ratings_rdd.map(lambda (u,m,r,t): m).distinct().collect()
-
-    # make LUTs for mapping ID to index
-    # user_LUT: userID -> i
-    # movie_LUT: movieID -> j
-    user_LUT = {id:idx for idx, id in enumerate(user_list)}
-    user_LUT_transpose = {idx:id for idx, id in enumerate(user_list)}
-    assert len(user_LUT) == len(user_list)
-    movie_LUT = {id:idx for idx, id in enumerate(movie_list)}
-    movie_LUT_transpose = {idx:id for idx, id in enumerate(movie_list)}
-    assert len(movie_LUT) == len(movie_list)
-    # userID: 2~672 -> 0~670
-    # movieID: 1~164979 -> 0~164978
 
     ''' make utility matrix '''
     # initialize M with NaN value
-    M = np.full(shape=(len(user_list), len(movie_list)), fill_value=np.nan, dtype=np.float)
+    M = np.full(shape=(671, 164979), fill_value=np.nan, dtype=np.float)
     
     # iterate over dataset and fill out utility matrix
     for u, m, r, t in ratings:
-        M[user_LUT[u], movie_LUT[m]] = r
-    
-    # normalize utility matrix
-    M, means = normalize_utility_matrix(M)
+        M[u-2, m-1] = r
+
+    np.save('matrix.npy', M)
 
     ''' top-10 similar users to U '''
-    U = user_LUT[600]
-    print U
-    top_ten_similar_users = sorted(range(M.shape[0]), key=lambda x: cosine(M[x], M[U]), reverse=True)[:10]
-    print top_ten_similar_users
+    # normalize utility matrix
+    normalized_M = normalize_utility_matrix(M)
 
+    # ID variables are denoted as index value
+    U = 600-2
+    top_ten_similar_users = sorted(range(normalized_M.shape[0]), key=lambda x: cosine(normalized_M[x, :], normalized_M[U, :]), reverse=True)[:10]
+    
+    ''' user-based collaborative filtering '''
     # slim utility matrix by top-10 similar users
     N = M[top_ten_similar_users, :]
-    means_N = means[top_ten_similar_users]
-
-    ''' user-based collaborative filtering '''
-    # iterate though columns 0~999 of matrix N, and fill out M[U, 0:999]
-    for j, movie_ratings in enumerate(N[:, [movie_LUT[j] for j in range(1, 1000, 1)]].T):
-        assert np.isnan(M[U, j])
-        M[U, j] = mean(movie_ratings)
     
-    sorted(range(len(M[U, 0:999])), key=lambda j: M[U, j], reverse=True)
+    # iterate though columns 0~999 of matrix N, and fill out M[U, 0:1000]
+    prediction = M[U, 0:1000].copy()
+    for j, movie_ratings in enumerate(N[:, 0:1000].T):
+        assert np.isnan(prediction[j])
+        prediction[j] = mean(movie_ratings)
+    
+    # print top-5 recommended movies among 1~1000
+    top_five_recommended_movies = np.argsort(-prediction)[:5]
+    for m in top_five_recommended_movies:
+        print '{}\t{}'.format( m + 1, prediction[m])
+    
+    ''' item-based collaborative filtering '''    
+    X = np.nan_to_num(normalized_M[:, 0:1000])
+    Y = np.nan_to_num(normalized_M[:, 1000:])
+
+    Z = np.matmul(
+        (X / (np.linalg.norm(X, axis=0) + 1e-6)).T,
+        (Y / (np.linalg.norm(Y, axis=0) + 1e-6)))
+    
+    Q = np.argsort(-Z, axis=1)[:, :10]
+    Q = Q + 1000
+
+    # 10 similar movies of I => Q[i, :]
+
+    for j in range(len(prediction)):
+        prediction[j] = mean(M[U, Q[j, :]])
+
+    # print top-five recommendtion using item-item collaborative filtering 
+    top_five_recommended_movies = np.argsort(-prediction)[:5]
+    for m in top_five_recommended_movies:
+        print '{}\t{}'.format( m + 1, prediction[m])
 
 
 if __name__ == '__main__':
